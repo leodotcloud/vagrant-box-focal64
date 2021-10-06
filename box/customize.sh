f004f405ebf832eb9630f5c558506c51c3418351
Murali Paluru <leodotcloud@gmail.com>
2021-10-06 19:28:26 +0530

    adding public ssh keys

--- box/customize.sh
+++ box/customize.sh
@@ -7,3 +7,6 @@ vim
 # Install docker
 export DEBIAN_FRONTEND=noninteractive;curl -sSL https://releases.rancher.com/install-docker/"${DOCKER_VERSION}".sh | sh -
 sudo usermod -aG docker vagrant
+
+# Add SSH public keys
+curl https://github.com/leodotcloud.keys >> ${HOME}/.ssh/authorized_keys
